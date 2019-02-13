661b42efb7312096573b7d4e109e2283ae419a38
Alexperkky <alex.perkinson5@gmail.com>
2019-02-12 19:30:44 -0800

    updated incorrect info, fixed issues

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Island Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Char', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Western', FALSE);
